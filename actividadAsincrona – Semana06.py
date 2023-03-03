4ac36602afecc7933fba6f1a511b23da1aa0e140
Alexander9753100 <126535792+alexander9753100@users.noreply.github.com>
2023-03-03 19:29:30 -0300

    Add files via upload

---
+++ actividadAsincrona – Semana06.py
@@ -0,0 +1,87 @@
+caratula = "Hola bienvenido anuestro segundo programa de fundamentos de programacion"
+print(caratula)
+
+caratula_ = "A continuacion se estara trabajando en sumas, restas, multiplicaciones, exponente, modulo, division, division entera"
+print(caratula_)
+
+p1 = "Sumar 3 variables distintas y asignar el valor a una cuarta variable"
+print(p1)
+su1 = 10
+su2 = 7
+su3 = 8
+suma = su1 + su2 + su3 
+print("10 + 7 + 8 =",suma)
+s4 = 25
+print("resultado de la suma:",s4)
+
+p2 = "Restar 4 variables siendo en la tercera variable más alta que la primera"
+print(p2)
+re1 = -6
+re2 = -8
+re3 = -10
+re4 = -12
+resta = re1 - re2 - re3 - re4
+print("6 - 8 - 10 - 12 =",resta)
+re5 = -24
+print("el resultado dela resta:",re5)
+
+p3 = "Multiplicar 2 variables asignarlas a otra variable y esa variable multiplicar por la segunda variable de suma y resta"
+print(p3)
+mu1 = 2
+mu2 = 3
+multiplicar = mu1 * mu2 
+print("2 x 3 =",multiplicar)
+mu3 = 6
+print(" el resultado dela multiplicacion es:",mu3)
+
+p3_ = " multiplicacion del resultado dela primera multiplicacion por la segunda variable de suma y resta"
+print(p3_)
+mu4 = mu3
+mu5 = su2
+mu6 = re2
+multiplicar = mu4 * mu5 * mu6
+print("6 x 7 x 8 =",multiplicar)
+mu6 = -336
+print("respuesta de la multiplicacion del resultado dela primera multiplicacion por la segunda variable de suma y resta es:",mu6)
+
+p4 = "Sacar a la primera variable de la multiplicación el exponente de la segunda variable de la multiplicación"
+print(p4)
+
+e1 = mu1
+e2 = mu2
+exponentes = e1 ** e2
+print("2**3=",exponentes)
+e3 = 8
+print(" el resultado de la multiplicacion de exponente es:",e3)
+
+p5 = "Sacar el módulo de la primera variable de la suma con la primera variable de la resta"
+print(p5)
+
+mo1 = su1
+mo2 = re1
+modulos = mo1 % mo2
+print("10 % -6 =",modulos)
+mo3 = -2
+print("el resultado de Sacar el módulo de la primera variable de la suma con la primera variable de la resta es:",mo3)
+
+p6 = "Dividir la variable resultado del exponente entre la variable resultado del módulo"
+print(p6)
+
+div1 = e3
+viv2 = mo3
+dividir = e3 / mo3
+print("8 / 4 =",dividir)
+D1 = -4.0
+print(" el resultado de Dividir la variable resultado del exponente entre la variable resultado del módulo es:",D1)
+
+p7 = "Realizar la división entera de la división normal"
+print(p7)
+divn1 = e3
+mo3 = mo3
+respuesta = e3 // mo3
+print("8 // - 2 =",respuesta)
+D2 = -4
+print("el resultado de la división entera de la división normal es:",D2)
+
+
+
